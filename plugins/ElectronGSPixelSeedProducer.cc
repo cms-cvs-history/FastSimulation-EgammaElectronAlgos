0f235e1950099e8d20c66fb92b4f0f42735b7098
cmsbuild <>
2008-05-14 02:24:24 +0000

    This commit was manufactured by cvs2git to create tag 'FastSimulation- EgammaElectronAlgos-V00-07-05-00'.
    Sprout from master 2008-05-14 02:24:22 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'updating py configs'
    Cherrypick from master 2008-04-29 18:06:54 UTC Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch> 'Make it work in 21X, with external seed collection (still needs testing)':
        plugins/ElectronGSPixelSeedProducer.cc
        plugins/ElectronGSPixelSeedProducer.h

--- plugins/ElectronGSPixelSeedProducer.cc
+++ plugins/ElectronGSPixelSeedProducer.cc
@@ -23,7 +23,7 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "Geometry/Records/interface/CaloGeometryRecord.h"
+#include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
 #include "Geometry/CaloGeometry/interface/CaloGeometry.h"
 #include "RecoEgamma/EgammaTools/interface/HoECalculator.h"
@@ -41,8 +41,6 @@
 
 ElectronGSPixelSeedProducer::ElectronGSPixelSeedProducer(const edm::ParameterSet& iConfig)
 {
-
-  m_firstTimeProduce = true ;
 
   edm::ParameterSet pset = iConfig.getParameter<edm::ParameterSet>("SeedConfiguration");
   SCEtCut_=iConfig.getParameter<double>("SCEtCut");
@@ -74,11 +72,11 @@ ElectronGSPixelSeedProducer::~ElectronGSPixelSeedProducer()
       delete matcher_;
 }
 
-void ElectronGSPixelSeedProducer::beginJobProduce(edm::EventSetup const&iSetup) 
+void ElectronGSPixelSeedProducer::beginJob(edm::EventSetup const&iSetup) 
 {
   // Get the calo geometry
   edm::ESHandle<CaloGeometry> theCaloGeom;
-  iSetup.get<CaloGeometryRecord>().get(theCaloGeom);
+  iSetup.get<IdealGeometryRecord>().get(theCaloGeom);
 
   // The H/E calculator
   calc_=HoECalculator(theCaloGeom);
@@ -91,12 +89,6 @@ void ElectronGSPixelSeedProducer::produce(edm::Event& e, const edm::EventSetup& 
 {
   LogDebug("entering");
   LogDebug("")  <<"[ElectronGSPixelSeedProducer::produce] entering " ;
-
-  if( m_firstTimeProduce )
-  {
-     beginJobProduce( iSetup ) ;
-     m_firstTimeProduce = false ;
-  }
 
   // get initial TrajectorySeeds if necessary
   if (fromTrackerSeeds_) {
